3b6e0a3696a85fa7fbbe24289c66fcf908b7280a
John Doe <john.doe@example.com>
2025-02-22 12:44:06 -0500

    testing comments for pushing

--- src/App.test.js
+++ src/App.test.js
@@ -1,5 +1,5 @@
 import { render, screen } from '@testing-library/react';
-import App from './App'; // testing changes
+import App from './App'; // testing changes test
 
 test('renders learn react link', () => {
   render(<App />);

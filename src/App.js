b54c543b656889cd05c93a66502a478fb2b642fa
John Doe <john.doe@example.com>
2025-02-22 13:45:04 -0500

    added the location fields and changed names

--- src/App.js
+++ src/App.js
@@ -2,15 +2,17 @@ import React, { useState } from "react";
 import "./firebaseConfig";
 import { getFirestore, addDoc, collection } from "firebase/firestore";
 function App() {
-  const [BusinessName, SetBusinessName] = useState("");
+  const [BusinessName, SetBusinessName] = useState("");       //variables that will be stored into the database
   const [Email, SetEmail] = useState("");
+  const [Location, SetLocation] = useState("");
 
   const db = getFirestore();
 
   const saveDataToFirestore = async () => {
-    const docRef = await addDoc(collection(db, "myCollection"), {
-      field1: BusinessName,
-      field2: Email,
+    const docRef = await addDoc(collection(db, "Businesses"), {
+      Name: BusinessName,
+      Email: Email,
+      Location: Location,
     });
     alert("in database now");
   };
@@ -58,6 +60,7 @@ function App() {
         />
       </div> */}
 
+        {/* ADDING THE EMAIL INFORMATION OF A BUSINESS HERE */}
         <div style={{ marginBottom: "10px" }}>
           <label>Email Address:</label>
           <input
@@ -69,8 +72,35 @@ function App() {
             style={{ marginLeft: "10px" }}
           />
         </div>
-        <button onClick={saveDataToFirestore}> Save to Firestore</button>
       </div>
+
+
+      {/* ADDING THE LOCATION FIELD OF THE BUSINESS HERE */}
+      <div style={{ marginBottom: "10px" }}>
+          <label>Location:</label>
+          <input
+            type="text"
+            name="location"
+            value={Location}
+            onChange={(e) => SetLocation(e.target.value)}
+            placeholder="Enter Address"
+            style={{ marginLeft: "10px" }}
+          />
+        </div>
+
+        {/* ADDING THE DATE CREATED */}
+        <div style={{ marginBottom: "10px" }}>
+          <label>Location:</label>
+          <input
+            type="text"
+            name="location"
+            value={Location}
+            onChange={(e) => SetLocation(e.target.value)}
+            placeholder="Enter Address"
+            style={{ marginLeft: "10px" }}
+          />
+        </div>
+        <button onClick={saveDataToFirestore}> Save to Firestore</button>
     </div>
   );
 }

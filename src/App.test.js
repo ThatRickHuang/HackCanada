09b7e6ace68d0d7394b7ee21d079decd61f937ac
John Doe <john.doe@example.com>
2025-02-22 12:28:23 -0500

    testing comment to see if comments work

--- src/App.test.js
+++ src/App.test.js
@@ -1,5 +1,5 @@
 import { render, screen } from '@testing-library/react';
-import App from './App';
+import App from './App'; // testing changes
 
 test('renders learn react link', () => {
   render(<App />);
